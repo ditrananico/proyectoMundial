19e21ce71fe30d71a7375982e4c394155c7eebdf
Nico <nicoditrana_96@hotmail.com>
2022-09-28 20:03:21 -0300

    initial commit

---
+++ src/main/java/ar/com/proyectomundial/ProyectoMundialApplication.java
@@ -0,0 +1,13 @@
+package ar.com.proyectomundial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoMundialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoMundialApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ar/com/proyectomundial/core/controller/testController.java
@@ -0,0 +1,11 @@
+package ar.com.proyectomundial.core.controller;
+
+//@RestController
+public class testController {
+
+  //  @GetMapping(value="/")
+    //@RequestMapping(value="/",method=RequestMethod.GET)
+    public String hello(){
+        return "Hello World!!";
+    }
+}
